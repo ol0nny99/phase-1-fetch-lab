096bf94b187e6322a0c5a5a63103fccee1d5e459
ol0nny99 <100658149+ol0nny99@users.noreply.github.com>
2022-05-08 18:02:21 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -14,4 +15,6 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
-});
+})
+
+
